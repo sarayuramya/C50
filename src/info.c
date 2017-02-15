ae15c452819b2dd6888cdf9f29331b7d056f45a5
sarayuramya <ramyaks1996@gmail.com>
2017-02-15 11:12:49 +0530

    Update info.c

--- src/info.c
+++ src/info.c
@@ -88,7 +88,7 @@ double TotalInfo(double V[], DiscrValue MinVal, DiscrValue MaxVal)
     DiscrValue	v;
     double	Sum=0.0, TotalCases=0;
     CaseCount	N;
- 	double alpha=-6.5;
+ 	double alpha=-2.5;
 	double r=1/(1-alpha);
 
     ForEach(v, MinVal, MaxVal)
