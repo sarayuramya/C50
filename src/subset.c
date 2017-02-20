b7a7ec52a5dc98246745f0bc23f0db85e1c27f4e
sarayuramya <ramyaks1996@gmail.com>
2017-02-20 10:46:41 +0530

    Update subset.c

--- src/subset.c
+++ src/subset.c
@@ -367,7 +367,7 @@ void Merge(DiscrValue x, DiscrValue y, CaseCount Cases)
     double	Entr1=0.0,count1=0.0;
     CaseCount	KnownCases=0;
     int		R, C;
-    double alpha=-2.25;
+    double alpha=-0.75;
     double q =1/(1-alpha);
     AddBlock(x, y);
 	double count[20];
@@ -441,7 +441,7 @@ void EvaluatePair(DiscrValue x, DiscrValue y, CaseCount Cases)
     ClassNo	c;
     double Entr1=0.0,Entr2=0.0,Entr3=0.0,count1=0.0;
     CaseCount	KnownCases=0, F;
-    double alpha=-2.25;
+    double alpha=-0.75;
     double q= 1/(1-alpha);
 	double count[20];
 	int i=0;
