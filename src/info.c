fd19e884eb297cdb42a05fdf62d88942eddc7ddc
sarayuramya <ramyaks1996@gmail.com>
2017-03-09 10:52:27 +0530

    Update info.c

--- src/info.c
+++ src/info.c
@@ -63,7 +63,7 @@ double count[20];
     ForEach(v, 1, MaxVal)
     {
 	ThisInfo += TotalInfo(GEnv.Freq[v], 1, MaxClass);
-	    count[i] += GEnv.Freq[x][v]-GEnv.Freq[y][v];
+	count[i] += GEnv.Freq[x][v]-GEnv.Freq[y][v];
     }
 	if(count[i]<0)
 	{
@@ -96,7 +96,7 @@ double TotalInfo(double V[], DiscrValue MinVal, DiscrValue MaxVal)
     DiscrValue	v,x,y;
     double	Sum=0.0, TotalCases=0,Sum1=0.0;
     CaseCount	N;
-double alpha =0.75;
+double alpha =1.30;
 	double count[20];
 	int i=0;
 	double q= 1/(1-alpha); 
@@ -106,19 +106,10 @@ double alpha =0.75;
 	N = V[v];
 	//Sum += N * Log(N);		 
         Sum += (pow(N,alpha));
-		TotalCases += N;
-	//count[i] += GEnv.Freq[x][v]-GEnv.Freq[y][v];	 
+	TotalCases += N;
     	}
-	/*if(count[i]<0)
-	{
-		count[i] = -1 * count[i];
-	}
-	count[i] /= TotalCases;*/
 	Sum = Sum /TotalCases;
 	Sum =(Sum -1)*q;	
-	//Sum *= count[i];
-	
-	//i++;
     return Sum;
 }
 
